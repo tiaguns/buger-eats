3537383ad65f8c4b77394a42478affb3751a6a4a
tiaguns <tiaguns@gmail.com>
2022-03-07 15:25:40 -0300

    buger-delivery

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -13,7 +13,18 @@ describe('Cadastro', ()=>{
             nome: 'Tiago Nascimento',
             cpf: '00000000025',
             email: 'tiaguns@gmail.com',
-            whats: '999999999'
+            whats: '999999999',
+            endereco: {
+                cep: '49082030',
+                rua: 'Rua Amador Bueno',
+                numero: '108',
+                complemento: 'ap 300',
+                bairro: 'Novo Paraíso',
+                cidade_uf: 'Aracaju/SE'
+
+                
+            },
+            metodo_entrega: 'Moto'
         }
 
        
@@ -21,6 +32,17 @@ describe('Cadastro', ()=>{
         cy.get('#page-deliver > form > fieldset:nth-child(2) > div:nth-child(2) > div:nth-child(2) > input[type=text]').type(entregador.cpf)
         cy.get('#page-deliver > form > fieldset:nth-child(2) > div:nth-child(3) > div:nth-child(1) > input[type=email]').type(entregador.email)
         cy.get('#page-deliver > form > fieldset:nth-child(2) > div:nth-child(3) > div:nth-child(2) > input[type=text]').type(entregador.whats)
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+        
+        cy.get('#page-deliver > form > fieldset:nth-child(3) > div:nth-child(4) > div:nth-child(1) > input[type=text]').type(entregador.endereco.numero)
+        cy.get('#page-deliver > form > fieldset:nth-child(3) > div:nth-child(4) > div:nth-child(2) > input[type=text]').type(entregador.endereco.complemento)
+
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains(' .delivery-method li', entregador.metodo_entrega).click()
 
 
 
